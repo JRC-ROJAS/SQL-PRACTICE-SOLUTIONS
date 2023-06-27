3762bcd96972288aaf466b66d7b200d135b758bf
JHONNY ROJAS CAMACHO <93992537+JRC-ROJAS@users.noreply.github.com>
2023-06-27 09:33:02 -0400

    Update QUESTIONS-EASI.sql

--- QUESTIONS-EASI.sql
+++ QUESTIONS-EASI.sql
@@ -2,3 +2,8 @@
 #Show first name, last name, and gender of patients whose gender is 'M'
 #SOLUTION NRO1
 SELECT first_name,last_name,gender FROM patients WHERE gender = "M";
+
+#QUESTION NRO2
+#Show first name and last name of patients who does not have allergies. (null)
+#SOLUTION NRO2
+SELECT first_name,last_name FROM patients WHERE allergies IS NULL;
